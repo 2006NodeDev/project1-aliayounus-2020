b0f6c40088d2095e7b4f5aca26622158a64d476b
Younus <alia.younus@accenture.com>
2020-07-20 11:03:03 -0500

    update

---
+++ project-1-ui/src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+//import logo from './logo.svg';
+import './App.css';
+import LoginComponent from './component/logincomponent';
+
+function App() {
+  return (
+    <div className="App">
+      
+      <LoginComponent/>
+    </div>
+  );
+}
+
+export default App;
