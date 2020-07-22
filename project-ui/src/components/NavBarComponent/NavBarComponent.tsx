b62b8894ef260cd6c62ac878c40a6f3338f2b8c7
Younus <alia.younus@accenture.com>
2020-07-22 11:54:01 -0500

    update

--- project-ui/src/components/NavBarComponent/NavBarComponent.tsx
+++ project-ui/src/components/NavBarComponent/NavBarComponent.tsx
@@ -1,15 +1,95 @@
+// import React, { FunctionComponent } from 'react';
+// import AppBar from '@material-ui/core/AppBar'
+// import Toolbar from '@material-ui/core/Toolbar';
+// import Typography from '@material-ui/core/Typography';
+// //import Button from '@material-ui/core/Button';
+// import IconButton from '@material-ui/core/IconButton';
+// import MenuIcon from '@material-ui/icons/Menu';
+// import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
+// import Menu from '@material-ui/core/Menu'
+// import MenuItem from '@material-ui/core/MenuItem'
+// import {Link} from 'react-router-dom'
+
+// //this is an example of Jss - a more js way of doing css
+// const useStyles = makeStyles((theme: Theme) =>
+//     createStyles({
+//         root: {
+//             flexGrow: 1,
+//         },
+//         menuButton: {
+//             marginRight: theme.spacing(2),
+//         },
+//         title: {
+//             flexGrow: 1,
+//         },
+//     }),
+// );
+// //this is the skeleton
+// export const NavBarComponent: FunctionComponent<any> = (props) => {
+//     const classes = useStyles();
+//     const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+//     const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+//         setAnchorEl(event.currentTarget);
+//     };
+//     const handleClose = () => {
+//         setAnchorEl(null);
+//     };
+//     //we can programmatically build the menu items
+//     let menuItems = []
+//     //always have the login item
+//    // menuItems.push(<MenuItem onClick={handleClose}><Link to='/login'>Login</Link></MenuItem>)
+//     if(props.user){
+//      menuItems.push(<MenuItem onClick={handleClose}><Link to={`/profile/${(props.user)?props.user.userId : '0' }`}>My Profile</Link></MenuItem>)
+//         //if they are logged in, add the other items
+//       
+//        // <MenuItem onClick={handleClose}><Link to={`/profile/${(props.user)?props.user.userId : '0' }`}>My Profile</Link></MenuItem>)
+//     }
+//     if(props.user && props.user.role === 'Admin'){
+//         menuItems.push(<MenuItem onClick={handleClose}><Link to='/users'>All Users</Link></MenuItem>,)
+//     }
+//     //  { if(props.user && props.user.role === 'Admin'){
+//     //      menuItems.push(<MenuItem onClick={handleClose}><Link to='/users'> Edit User </Link></MenuItem>,)}
+//     //  }
+//     // if (props.user && props.user.role.role === 'admin'){
+//     //     menuItems.push([<MenuItem onClick={handleClose}> <Link to= {`/MyProfile/${(props.user)?props.user.userId : '0' }`} > My Profile </Link> </MenuItem>,
+//     //     <MenuItem onClick={handleClose}> <Link to='/EditMyProfile' > Edit My Profile </Link> </MenuItem>,
+//     return (
+//         <nav>
+
+//             <AppBar position="static">
+//                 <Toolbar>
+//                     <IconButton onClick={handleClick} edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+//                         <MenuIcon />
+//                     </IconButton>
+//                     <Menu id="simple-menu"
+//                         anchorEl={anchorEl}
+//                         keepMounted
+//                         open={Boolean(anchorEl)}
+//                         onClose={handleClose}>
+//                         {menuItems}
+//                     </Menu>
+//                     <Typography variant="h2" className={classes.title}>
+//                         Welcome to Userbook
+//                 </Typography>
+//                 </Toolbar>
+//             </AppBar>
+//         </nav>
+//         
+//     )
+// }
+
+
 import React, { FunctionComponent } from 'react';
 import AppBar from '@material-ui/core/AppBar'
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
-//import Button from '@material-ui/core/Button';
+import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
 import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
 import Menu from '@material-ui/core/Menu'
 import MenuItem from '@material-ui/core/MenuItem'
 import {Link} from 'react-router-dom'
-//import { LoginComponent } from '../LoginComponent/LoginComponent';
 
 //this is an example of Jss - a more js way of doing css
 const useStyles = makeStyles((theme: Theme) =>
@@ -38,27 +118,21 @@ export const NavBarComponent: FunctionComponent<any> = (props) => {
     const handleClose = () => {
         setAnchorEl(null);
     };
-
     //we can programmatically build the menu items
     let menuItems = []
     //always have the login item
     menuItems.push(<MenuItem onClick={handleClose}><Link to='/login'>Login</Link></MenuItem>)
     if(props.user){
         //if they are logged in, add the other items
-      
-        <MenuItem onClick={handleClose}><Link to={`/profile/${(props.user)?props.user.userId : '0' }`}>My Profile</Link></MenuItem>
+        menuItems.push(<MenuItem onClick={handleClose}><Link to='/clicker'>Clicker</Link></MenuItem>,
+        //<MenuItem onClick={handleClose}><Link to='/first'>First</Link></MenuItem>,
+        //<MenuItem onClick={handleClose}><Link to='/title'>Title</Link></MenuItem>,
+        <MenuItem onClick={handleClose}><Link to={`/profile/${(props.user)?props.user.userId : '0' }`}>My Profile</Link></MenuItem>)
     }
     if(props.user && props.user.role === 'Admin'){
         menuItems.push(<MenuItem onClick={handleClose}><Link to='/users'>All Users</Link></MenuItem>,)
     }
 
-    //  { if(props.user && props.user.role === 'Admin'){
-    //      menuItems.push(<MenuItem onClick={handleClose}><Link to='/users'> Edit User </Link></MenuItem>,)}
-    //  }
-
-    // if (props.user && props.user.role.role === 'admin'){
-    //     menuItems.push([<MenuItem onClick={handleClose}> <Link to= {`/MyProfile/${(props.user)?props.user.userId : '0' }`} > My Profile </Link> </MenuItem>,
-    //     <MenuItem onClick={handleClose}> <Link to='/EditMyProfile' > Edit My Profile </Link> </MenuItem>,
 
     return (
         <nav>
@@ -76,13 +150,12 @@ export const NavBarComponent: FunctionComponent<any> = (props) => {
                         onClose={handleClose}>
                         {menuItems}
                     </Menu>
-                    <Typography variant="h2" className={classes.title}>
-                        Welcome to Userbook
+                    <Typography variant="h6" className={classes.title}>
+                        Welcome to userbook
                 </Typography>
+                    <Button color="inherit">Login</Button>
                 </Toolbar>
             </AppBar>
         </nav>
-        
     )
 }
-
